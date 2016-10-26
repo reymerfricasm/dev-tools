f528657d63ec717b7135cdc7bd325a56fa4c3989
Reymer Fricas Moscoso <reymerfmoscoso@gmail.com>
2016-10-25 22:31:25 -0400

    Agregar archivos de proyecto.

---
+++ dev-tools/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="dev_tools.MvcApplication" Language="C#" %>
---
+++ dev-tools/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(dev_tools.Startup))]
+namespace dev_tools
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
